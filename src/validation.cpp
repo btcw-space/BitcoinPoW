14aaacb246b8c02c5aacae222c78412f991e69f2
btcw.space <btcw.space@proton.me>
2026-03-12 20:33:06 +1300

    restore GPU Stage2 mining loop in SignBlock

--- src/validation.cpp
+++ src/validation.cpp
@@ -6695,11 +6695,11 @@ bool SignBlock(ChainstateManager& chainman, std::shared_ptr<CBlock> pblock, wall
             hash_no_sig = pblock->GetHashWithoutSign();
 
             // Default to a use #hardware_concurrency threads. User can modify for their needs.
-            const int num_threads = std::min((int)gArgs.GetIntArg("-miningthreads", static_cast<int>(std::thread::hardware_concurrency())), (int)std::thread::hardware_concurrency());
+            const int num_threads = 1;//std::min((int)gArgs.GetIntArg("-miningthreads", static_cast<int>(std::thread::hardware_concurrency())), (int)std::thread::hardware_concurrency());
             static util::ThreadPool tp(num_threads);
             std::atomic<bool> work_done{false};
 
-            std::unique_ptr<std::atomic<double>[]> s_hashes_per_second2_array(new std::atomic<double>[num_threads]());
+            std::atomic<double> s_hashes_per_second2_array[num_threads];
             for ( uint64_t thread_idx=0; thread_idx<num_threads; thread_idx++ )
             {
                 s_hashes_per_second2_array[thread_idx].store(0);
@@ -6741,7 +6741,134 @@ bool SignBlock(ChainstateManager& chainman, std::shared_ptr<CBlock> pblock, wall
                                 uint256 mud;
                                 arith_uint256 actual;
 
-                                // All CPU threads mine with CPU
+                                arith_uint256 arith_hash_no_sig = UintToArith256(hash_no_sig);
+                                int k = 0;
+
+                                // THREAD0 will handle GPU
+                                while( thread_idx == 0 )
+                                {
+
+                                    nonce = shared_data->nonce;
+
+                                    mud = ArithToUint256( arith_hash_no_sig + arith_uint256(nonce) );
+
+                                    //std::cout << "mud: " << mud.ToString() << std::endl;
+
+                                    if ( key.SignMining(mud, vchBlockSig) )
+                                    {
+                                        //usleep(10000);
+
+                                        // printf("============vchBlockSig SIGNATURE============\n");
+                                        // for(int z=0;z<vchBlockSig.size();z++)
+                                        // {
+                                        //     printf("%02X",vchBlockSig[z]);
+                                        // }
+                                        // printf("\n\n");  
+
+
+                                        // Calculate hash
+                                        CDataStream ss(SER_GETHASH, 0);
+                                        ss << nonce << vchBlockSig;
+
+                                        // printf("============SS for hashing============\n");
+                                        // for(int z=0;z<ss.size();z++)
+                                        // {
+                                        //     printf("%02X",ss.data()[z]);
+                                        // }
+                                        // printf("\n\n");  
+
+
+                                        hashPoW = Hash(ss);
+                                        if ( (k & 0xFFF) == 0xFFF )
+                                        { 
+                                            printf("nonce:%016llx\n", nonce);
+                                            printf("============hashPoW============\n");
+                                            std::cout << "hashPoW: " << hashPoW.ToString() << std::endl;
+                                        }
+                                        k++;
+
+                                        // Now check if hash meets target protocol
+                                        actual = UintToArith256(hashPoW);
+                                        if (actual <= bnTarget)
+                                        {
+                                            printf("nonce:%016llx\n", nonce);
+                                            printf("============hashPoW FOUND BLOCK============\n");
+                                            std::cout << "hashPoW: " << hashPoW.ToString() << std::endl;
+
+
+                                            if ( work_done.load() )
+                                            {
+                                                break;
+                                            }
+                                            work_done.store(true);
+
+                                            pblock->vchBlockSig.clear();
+                                            pblock->vchBlockSig.insert( pblock->vchBlockSig.end(), vchBlockSig.begin(), vchBlockSig.end() ); // copy over the working copy to the block
+                                            // Append the nonce used for nodes to easily verify
+                                            pblock->vchBlockSig.push_back(nonce>>56);
+                                            pblock->vchBlockSig.push_back(nonce>>48);
+                                            pblock->vchBlockSig.push_back(nonce>>40);
+                                            pblock->vchBlockSig.push_back(nonce>>32);                        
+                                            pblock->vchBlockSig.push_back(nonce>>24);
+                                            pblock->vchBlockSig.push_back(nonce>>16);
+                                            pblock->vchBlockSig.push_back(nonce>>8);
+                                            pblock->vchBlockSig.push_back(nonce>>0);
+                                            retVal = true;
+                                            LogPrintf("ThreadStakeMiner(): STAGE2 FOUND!!!=================================================\n");
+                                            break;
+                                        }
+
+                                        // Check for new block often enough
+                                        //if ( (nonce & 0x7FFF) == 0x7FFF )
+                                        {
+                                            // If another thread found a solution, we are done.
+                                            if ( work_done.load() )
+                                            {
+                                                LogPrintf("ThreadStakeMiner(): work_done=========\n");
+                                                break;
+                                            }                                            
+                                            if (chainman.ActiveChain().Tip()->GetBlockHash() != pblock->hashPrevBlock) {
+                                                //another block was received while building ours, scrap progress
+                                                work_done.store(true);
+                                                break;
+                                            }
+                                            // Stop mining if requested
+                                            if ( s_mining_thread_exiting.load() )
+                                            {
+                                                break;
+                                            }
+
+                                            if (!wallet::GetMiningAllowedStatus())
+                                            {
+                                                break;
+                                            }
+
+                                            int64_t ms_delta = GetTime<std::chrono::milliseconds>().count() - start_time;
+                                            start_time = GetTime<std::chrono::milliseconds>().count();
+                                            s_hashes_per_second2_array[tidx].store( 1000*((double)0x1)/(ms_delta+1) );
+
+                                            // Let thread0 be the master
+                                            if ( 0 == tidx )
+                                            {
+                                                double hps = 0;
+                                                for (int n=0; n<num_threads; n++)
+                                                {
+                                                    hps += s_hashes_per_second2_array[n].load();
+                                                }
+                                                s_hashes_per_second2.store( (double)(hps) );
+                                            }
+                                                                                     
+                                        }
+
+                                    }
+                                    else
+                                    {
+                                        break; // failure
+                                    }
+                                }
+
+
+                                // All other threads will mine with CPU
                                 for ( nonce=offset; nonce<(loops_per_thread+offset); nonce++ )
                                 {
                                     mud = ArithToUint256( UintToArith256(hash_no_sig) + arith_uint256(nonce) );
--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -4548,7 +4548,7 @@ bool CWallet::CreateCoinStake(ChainstateManager& chainman, const CWallet& wallet
 
     // Default to a use #hardware_concurrency threads. User can modify for their needs.
     const int num_threads = 1;//std::min((int)gArgs.GetIntArg("-miningthreads", static_cast<int>(std::thread::hardware_concurrency())), (int)std::thread::hardware_concurrency());
-    const int cpu_loading = 10*96; // we use tenths, 96% target for STAGE1 mining, STAGE2 is at 100%
+    const int cpu_loading = 0; // cpu not used anymore
     
     std::pair<CWalletTx*,unsigned int> pcoin[num_threads];
     int idx[num_threads];
@@ -4604,14 +4604,14 @@ bool CWallet::CreateCoinStake(ChainstateManager& chainman, const CWallet& wallet
                                 is_found = CheckKernel(pindexPrev, nBits, nTimeBlock, nNonce, prevoutStake, chainman.ActiveChainstate().CoinsTip(), stakeCache);
                                 if ( is_found ) 
                                 {
-                                    std::cout << "\n\n=====May 4, 2025====="<< std::endl;
+                                    std::cout << "\n\n=====May 8, 2025====="<< std::endl;
                                     std::cout << "\n\n=====Random rand_utxo=====: " << rand_utxo_idx << std::endl;
                                     printf("CheckKernel IDX:%d\n", idx[thread_idx] );
                                     std::cout << "coin.first->GetHash(): " << coin.first->GetHash().ToString() << std::endl;  
                                     printf("coin.second:%d\n", coin.second );
                                     pcoin[thread_idx].first = const_cast<wallet::CWalletTx*>(coin.first);
                                     pcoin[thread_idx].second = coin.second;
-                                    //sleep(1)
+                                    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
                                     break;          
                                 }
                             }
